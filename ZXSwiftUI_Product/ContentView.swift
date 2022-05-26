bc52d32e3c3db30f9a3b19cc62d6a38cc9bbcb5f
gitXiaoWangZi <15538509549@163.com>
2022-05-26 10:58:39 +0800

    Initial Commit

---
+++ ZXSwiftUI_Product/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZXSwiftUI_Product
+//
+//  Created by 刘圣洁 on 2022/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZXSwiftUI_Product/ZXSwiftUI_ProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZXSwiftUI_ProductApp.swift
+//  ZXSwiftUI_Product
+//
+//  Created by 刘圣洁 on 2022/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ZXSwiftUI_ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
